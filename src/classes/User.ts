a753f0972a1ceec7b2d42319a257cf22bb9800c8
samriek <sem.leeuwin@hva.nl>
2025-01-15 14:07:07 +0100

    class voor user

---
+++ src/classes/User.ts
@@ -0,0 +1,11 @@
+class User {
+    public name: string;
+    public email: string;
+    public password: string;
+
+    public constructor(name: string, email: string, password: string) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
